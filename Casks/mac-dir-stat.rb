0614f742b06a46358f6497b44bc334486d7f8514
Pavol Dravecky <pavol@mini.local>
2026-05-03 00:04:51 +0200

    mac-dir-stat 0.3.0

--- Casks/mac-dir-stat.rb
+++ Casks/mac-dir-stat.rb
@@ -1,6 +1,6 @@
 cask "mac-dir-stat" do
-  version "0.2.2"
-  sha256 "8e9c52f50a6d9dce221d4848d7c6e1da3dc7bd5fafbb4907a9f1708b66cf64cf"
+  version "0.3.0"
+  sha256 "94f76ac9d7142cbba992a4f37219be9f74f46c5111a670373fa66dfdc3bd69fd"
 
   url "https://github.com/Chartres/mac-dir-stat/releases/download/v#{version}/MacDirStat-#{version}.dmg"
   name "MacDirStat"
